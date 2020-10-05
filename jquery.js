a3625487a1814a1f1e8dbf96e20030ed0338cec5
Kawsar Hossain <khossain227@gmail.com>
2020-10-05 19:49:38 +0600

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -39,3 +39,23 @@ $("#myInput").on("keyup", function() {
         $("#imgInp").change(function(){
           showImagePreview(this);
         });
+
+
+// copy from input field
+
+var copyButton = document.querySelector('.copy button');
+    var copyInput = document.querySelector('.copy input');
+    copyButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      var text = copyInput.select();
+      document.execCommand('copy');
+    });
+    copyInput.addEventListener('click', function() {
+      this.select();
+    });
+
+
+// filter input box for number
+ $('#donateAmount').on('keyup paste',function(){
+      this.value = this.value.replace(/[^0-9]/g, '');
+    })
